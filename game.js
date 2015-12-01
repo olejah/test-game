18f31873636c9388683ab5fe352e5dc612d25750
Oleg Gorovyi <o.gorovyi@pulsarfour.com>
2015-12-01 15:51:03 +0200

    commit 1_1

--- game.js
+++ game.js
@@ -1,46 +1,60 @@
 /**
  * Created by Олег on 01.12.15.
  */
-(function (game) {
-    var game = game || {},
+var background = new Image();
+background.src = "img/bg.jpg";
 
-        init = function () {
+var tiles = new Image();
+//tiles.src = "img/player.jpg";
 
-            game.map = document.getElementById("map");
-            game.ctxMap = game.map.getContext("2d");
+var game = {},
+    init = function () {
 
-            game.drawBtn = document.getElementById("drawBtn");
-            game.clearBtn = document.getElementById("clearBtn");
+        game.map = document.getElementById("map");
+        game.ctxMap = game.map.getContext("2d");
 
-            game.map.width = game.width = 600;
-            game.map.height = game.height = 270;
+        game.player = document.getElementById("player");
+        game.ctxPlayer = game.player.getContext("2d");
 
-            game.drawBtn.onclick = actions.drawRect;
-            game.clearBtn.onclick = actions.clearRect;
+        //game.drawBtn = document.getElementById("drawBtn");
+        //game.clearBtn = document.getElementById("clearBtn");
 
-            game.background = new Image();
-            game.background.src = "bg.jpg";
+        game.map.width = game.width = 600;
+        game.map.height = game.height = 270;
+
+        game.player.height = 120;
+        game.player.width = 90;
+
+        //game.drawBtn.onclick = actions.drawRect;
+        //game.clearBtn.onclick = actions.clearRect;
+
+        //  background = new Image();
+        // background.src = game.background.currentSrc = "bg.jpg";
+        actions.drawBg();
+        actions.drawPlayer();
+
+    },
+
+    actions = {
+        drawRect: function () {
+            game.ctxMap.fillStyle = "#3D3D3D";
+            game.ctxMap.fillRect(10, 10, 50, 50);
             actions.drawBg();
         },
-
-        actions = {
-            drawRect: function () {
-                game.ctxMap.fillStyle = "#3D3D3D";
-                game.ctxMap.fillRect(10, 10, 50, 50);
-            },
-            clearRect: function () {
-                game.ctxMap.clearRect(0, 0, 600, 270);
-            },
-
-            drawBg: function () {
-                console.log(game.background);
-                game.ctxMap.drawImage(game.background, 0, 0, game.width, game.height,
-                    0, 0, game.width, game.height);
-            }
-        };
-
-        window.onload = function(){
-            init();
+        clearRect: function () {
+            game.ctxMap.clearRect(0, 0, 600, 270);
+        },
+        drawBg: function () {
+            game.ctxMap.drawImage(background, 0, 0, game.width, game.height,
+                0, 0, game.width, game.height);
+        },
+        drawPlayer: function () {
+            game.ctxPlayer.drawImage(tiles, 0, 0, 120, 90, 0, 0, 120, 90);
         }
-})();
+    };
+
+window.onload = function () {
+    init();
+};
+
 
