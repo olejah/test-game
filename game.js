6a0b25717ae3cfcb5bb905bffd90e859d94d4b40
Oleg Gorovyi <o.gorovyi@pulsarfour.com>
2015-12-01 14:10:53 +0200

    commit 1_0

--- game.js
+++ game.js
@@ -1,25 +1,46 @@
 /**
  * Created by Олег on 01.12.15.
  */
-window.onload = init;
+(function (game) {
+    var game = game || {},
 
-var game = {};
+        init = function () {
 
-var init = function () {
-    game.map = document.getElementById("map");
-    game.ctxMap = game.map.getContext("2d");
-    game.drawBtn = document.getElementById("drawBtn");
-    game.clearBtn = document.getElementById("clearBtn");
+            game.map = document.getElementById("map");
+            game.ctxMap = game.map.getContext("2d");
 
-    drawRect.addEventListener("click", drawRect(), false);
-    clearRect.addEventListener("click", clearRect(), false);
-};
+            game.drawBtn = document.getElementById("drawBtn");
+            game.clearBtn = document.getElementById("clearBtn");
 
-function drawRect(){
-    game.ctxMap.fillStyle = "#303030";
-    game.ctxMap.fillRect(10,10,100,100);
-}
+            game.map.width = game.width = 600;
+            game.map.height = game.height = 270;
 
-function clearRect(){
-    game.ctxMap.clearRect(0,0,800,500)
-}
+            game.drawBtn.onclick = actions.drawRect;
+            game.clearBtn.onclick = actions.clearRect;
+
+            game.background = new Image();
+            game.background.src = "bg.jpg";
+            actions.drawBg();
+        },
+
+        actions = {
+            drawRect: function () {
+                game.ctxMap.fillStyle = "#3D3D3D";
+                game.ctxMap.fillRect(10, 10, 50, 50);
+            },
+            clearRect: function () {
+                game.ctxMap.clearRect(0, 0, 600, 270);
+            },
+
+            drawBg: function () {
+                console.log(game.background);
+                game.ctxMap.drawImage(game.background, 0, 0, game.width, game.height,
+                    0, 0, game.width, game.height);
+            }
+        };
+
+        window.onload = function(){
+            init();
+        }
+})();
+
